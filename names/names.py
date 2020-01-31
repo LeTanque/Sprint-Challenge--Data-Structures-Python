aa2ecebbc2c35d6faeac2225ec4fc71a4e362acc
Frank Martinez <frankie.martinez@gmail.com>
2020-01-31 15:07:50 -0800

    ✨ Attempt at stretch

--- names/names.py
+++ names/names.py
@@ -10,20 +10,26 @@ read_names_2 = open('names/names_2.txt', 'r')
 names_2 = read_names_2.read().split("\n")  # List containing 10000 names
 read_names_2.close()
 
-# init empty list for duplicates
-duplicates = []
-# init bst with empty string starting place
-bst = BinarySearchTree("")
 
-# for each name in the first list, insert it into the bst
-for name_1 in names_1:
-    bst.insert(name_1)
+def find_dups (set_1, set_2):
+    # init empty list for duplicates
+    dups = []
+    # init bst with empty string starting place
+    bst = BinarySearchTree("")
 
-# for each name in the second list, check for it in the bst
-for name_2 in names_2:
-    if bst.contains(name_2):
-        duplicates.append(name_2)
+    # for each name in the first list, insert it into the bst
+    for name_1 in set_1:
+        bst.insert(name_1)
 
+    # for each name in the second list, check for it in the bst
+    for name_2 in set_2:
+        if bst.contains(name_2):
+            dups.append(name_2)
+    
+    return dups
+
+
+duplicates = find_dups(names_1, names_2)
 
 # Timer End, print results
 end_time = time.time()
@@ -33,7 +39,6 @@ print (f"runtime: {end_time - start_time} seconds")
 # 64 duplicates
 # Current runtime: 13.01737...
 # Polynominal time O(n^c)
-# ---------- Stretch Goal -----------
-# Python has built-in tools that allow for a very efficient approach to this problem
-# What's the best time you can accomplish with no restrictions on techniques or data
-# structures?
+
+# improved runtime: 0.20588421821594238 seconds
+# Linear, I supposed. BST complexity.
